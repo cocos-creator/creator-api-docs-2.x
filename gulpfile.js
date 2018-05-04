059b378b5f233a182a423465c6f1e24948c524db
Jare Guo <jare85@163.com>
2018-05-04 23:39:55 +0800

    use firedoc to generate markdown

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 'use strict';
 
-// const Fs = require('fire-fs');
-const Path = require('path');
-const Del = require('del');
-// const spawn = require('child_process').spawn;
-// const gulpSequence = require('gulp-sequence');
-
+const fs = require('fs-extra');
+const { join, resolve } = require('path');
+const { spawn } = require('child_process');
+const del = require('del');
 const gulp = require('gulp');
+const es = require('event-stream');
 const program = require('commander');
 
 gulp.task('publish', function (done) {
@@ -18,9 +17,9 @@ gulp.task('publish', function (done) {
         return done('dest not supplied');
     }
 
-    var del = Path.join(dest, '*');
+    var del = join(dest, '*');
     console.log('deleting ' + del);
-    Del.sync(del, { force: true });
+    del.sync(del, { force: true });
 
     console.log('copying _book/**/* to ' + dest);
     gulp.src('_book/**/*', {
@@ -28,4 +27,91 @@ gulp.task('publish', function (done) {
         })
         .pipe(gulp.dest(dest))
         .on('end', done);
+});
+
+
+gulp.task('cp-apisrc', function () {
+    program
+        .option('--engine <path to engine>')
+        .parse(process.argv);
+
+    var engine = program.engine;
+
+    del.sync('lib/temp-src/engine/*');
+
+    fs.copySync(join(engine, 'docs'), 'lib/temp-src/engine/docs');
+
+    const DefaultModuleHeader =
+        '/**\n' +
+        ' * @module cc\n' +
+        ' */\n';
+    const LinkTagRE = /({\s*@link\s+)\/*(.+})/g;
+
+    var apisrc = fs.readJsonSync(join(engine, 'docs', 'apisrc.json'));
+    return gulp.src(apisrc, {
+            cwd: engine,
+            base: engine,
+            nodir: true
+        })
+        .pipe(es.through(function (file) {
+            var content = file.contents.toString();
+
+            // rebase example link to temp-src/engine/docs
+            content = content.replace(LinkTagRE, (m, p1, p2) => p1 + 'temp-src/engine/docs/' + p2);
+
+            // set defualt module as cc
+            content = DefaultModuleHeader + content;
+
+            file.contents = new Buffer(content);
+            this.emit('data', file);
+        }))
+        .pipe(gulp.dest('lib/temp-src/engine'));
+});
+
+gulp.task('build-md', ['cp-apisrc'], function (cb) {
+
+    // del old files
+
+    program
+        .option('--lang <language>')
+        .parse(process.argv);
+
+    var dest = resolve(program.lang);
+
+    del.sync(join(dest, 'assets'), { force: true });
+    del.sync(join(dest, 'classes'), { force: true });
+    del.sync(join(dest, 'enums'), { force: true });
+    del.sync(join(dest, 'modules'), { force: true });
+
+    // generate md
+
+    var args = [
+        '../node_modules/firedoc/bin/firedoc.js',
+        'build',
+        resolve('lib/temp-src'),
+        '-M',
+        '--theme',
+        resolve('lib/firedoc-theme/markdown'),
+        '-L',
+        program.lang,
+        '-D',
+        dest
+    ];
+    // console.log('running node with args', args);
+    var child = spawn('node', args, {
+        stdio: 'inherit',
+        cwd: 'lib'
+    });
+    child.on('exit', function (err) {
+        if (err) {
+            return cb(err);
+        }
+
+        // attach editor api
+
+        var indexPath = join(dest, 'index.md');
+        var editorIndexPath = 'editor-index.md';
+        var content = fs.readFileSync(indexPath, 'utf8') + '\n' + fs.readFileSync(editorIndexPath, 'utf8');
+        fs.writeFileSync(indexPath, content, 'utf8');
+    });
 });
